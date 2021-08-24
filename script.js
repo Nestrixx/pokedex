8716c89fe40bdfb8330984b87055beef691bfcf4
crimtin <christianmck@hotmail.com>
2021-08-24 00:44:08 -0500

    added benji button and added wrapper

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const q = document.getElementById('pokemon-types');
+const typeSelector = document.getElementById('pokemon-types');
+let pokemonArray = [];
+window.onload = function getPokemonList() {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then(response => response.json())
+        .then(pokemonList => {
+            pokemonList.results.forEach((pokemon) => {
+                fetch(pokemon.url).then(response => response.json())
+                    .then(individualPokemon => {
+                        pokemonArray.push(individualPokemon);
+                    })
+            })
+        })
+    console.log(pokemonArray);
+}
 
 function filterByType() {
-    console.log(q.value);
+    console.log(typeSelector.value);
 }
-q.onchange = filterByType;
+typeSelector.onchange = filterByType;
+
+function showMeData() {
+    console.log(pokemonArray)
+}
