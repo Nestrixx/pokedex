5e425f0e90e84a2c9c4f080090da07089548d7cf
crimtin <christianmck@hotmail.com>
2021-09-14 00:05:51 -0500

    added functionality to filterByType function now filters

--- script.js
+++ script.js
@@ -57,14 +57,34 @@ function getPokemonTypes() {
 
 function filterByType(event) {
     const selectedType = event.target.value;
-    let filteredPokemonList = [];
-    for (let i = 0; i < globalPokeList.length; i++) {
-        const pokemon = globalPokeList[i];
-        for (let q = 0; q < pokemon.types.length; q++) {
-            const pokemonType = pokemon.types[q].type.name;
-            if (pokemonType === selectedType) {
-                console.log(pokemon.name);
-            }
-        }
+    const newPokeArray = globalPokeList.filter(pokemon => {
+        return pokemon.types.find(typeObj => {
+            return typeObj.type.name === selectedType;
+        })
+    })
+
+    const pokemonList = document.getElementsByClassName('pokemonWrapper')[0];
+    pokemonList.innerHTML = '';
+
+
+    for (i = 0; i < newPokeArray.length; i++) {
+        const pokemonCard = document.createElement('div');
+        pokemonCard.className = "pokemonCard";
+
+        const artworkWrapper = document.createElement('div');
+        artworkWrapper.className = "artworkWrapper";
+
+        const pokemonImage = document.createElement('img');
+        let pokemonName = newPokeArray[i].name;
+        pokemonImage.src = `https://img.pokemondb.net/artwork/large/${pokemonName}.jpg`;
+        pokemonImage.className = "pokemonImg";
+
+        const pokemonParagraghElement = document.createElement('p');
+        pokemonParagraghElement.textContent += newPokeArray[i].name;
+
+        pokemonCard.appendChild(artworkWrapper);
+        artworkWrapper.appendChild(pokemonImage);
+        pokemonCard.appendChild(pokemonParagraghElement);
+        pokemonList.appendChild(pokemonCard);
     }
 }
